181b142659236ffe6a1f8dbb007ed933613f61d3
OhSeungIk <opp2556@gmail.com>
2022-01-19 10:41:00 +0900

    Hello Component,  App에서 Hello Component 불러오는 sample 추가.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Hello from './components/Hello/Hello';
 
 import "./css/index.scss";
 
 const App = () => {
     return (
         <>
-        test
+        <Hello/>
         </>
     )
 }
