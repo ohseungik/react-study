96bc843289ae29bda27b1d1db45a4ac81611ff23
OhSeungIk <opp2556@gmail.com>
2022-01-19 11:41:27 +0900

    InputSample Component로 useState Input String 관련 sample 추가.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './components/Counter/Counter';
 import Hello from './components/Hello/Hello';
+import InputSample from './components/InputSample/InputSample';
 import Wrapper from './components/Wrapper/Wrapper';
 
 import "./css/index.scss";
@@ -13,6 +14,8 @@ const App = () => {
             <Hello color="red"></Hello>
 
             <Counter/>
+
+            <InputSample/>
         </Wrapper>
     )
 }
