38b563c7c0fad2356df14dde6a1e4cdd604669e0
DESKTOP-72DPPI6\opp25 <opp2555@wizvera.com>
2022-01-19 10:26:53 +0900

    typescript + react + babel + webpack 환경 구축.

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./css/index.scss";
+
+const App = () => {
+    return (
+        <>
+        test
+        </>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
