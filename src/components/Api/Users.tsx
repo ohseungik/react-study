d1b94dd105ddd53fd75c1febc28a531722d72f31
OhSeungIk <opp2556@gmail.com>
2022-01-24 16:34:46 +0900

    axios Reducer로 가져오도록 sample 변경.

--- src/components/Api/Users.tsx
+++ src/components/Api/Users.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useReducer, useState } from "react";
 import axios from 'axios';
 
 interface user {
@@ -7,28 +7,53 @@ interface user {
     name: string
 }
 
+const UserReducer = (state: any, action: { type: any; data: any; error: any; }) => {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null
+            }
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            }
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error
+            }
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+}
+
 const Users = () => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(UserReducer, {
+        loading: false,
+        data: null,
+        error: null
+    })
 
     const fetchUsers = useCallback(async () => {
         try {
-            setError(null);
-            setUsers([]);
-            setLoading(true);
+            dispatch({ type: 'LOADING', data: null, error: null });
             const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-            setUsers(response.data);
+            dispatch({ type: 'SUCCESS', data: response.data, error: null });
         } catch (e: any) {
-            setError(e);
+            dispatch({ type: 'ERROR', data: null, error: e });
         }
-
-        setLoading(false);
     }, []);
 
     useEffect(() => {
         fetchUsers();
     }, []);
+
+    const { loading, data: users, error } = state;
 
     if (loading) return <div>로딩중...</div>;
     if (error) return <div>에러가 발생했습니다.</div>;
